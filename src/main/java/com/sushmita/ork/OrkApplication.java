0b4e89b3d91fd882ad7131adc4f0b007629f8094
sushmitabudhathoki <sushmita.budhathoki56@gmail.com>
2024-08-22 19:59:11 -0400

    files

---
+++ src/main/java/com/sushmita/ork/OrkApplication.java
@@ -0,0 +1,13 @@
+package com.sushmita.ork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sushmita/ork/OrkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sushmita.ork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
