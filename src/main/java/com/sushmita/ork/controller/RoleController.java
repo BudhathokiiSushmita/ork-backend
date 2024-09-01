bc3d0792b6743daa5e3f2fd65b9868459a31af07
sushmitabudhathoki <sushmita.budhathoki56@gmail.com>
2024-09-01 17:18:59 -0400

    role controller creation

---
+++ src/main/java/com/sushmita/ork/controller/RoleController.java
@@ -0,0 +1,18 @@
+package com.sushmita.ork.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.sushmita.ork.constants.APIConstants.ROLE_API;
+
+/**
+ * @author Sushmita Budhathoki on 2024-09-01
+ */
+
+
+@RestController
+@RequestMapping(ROLE_API)
+public class RoleController {
+
+
+}
