09b2e01176ce6604b2a964a731bbae9ddbec8e9a
sushmitabudhathoki <sushmita.budhathoki56@gmail.com>
2024-09-01 22:27:53 -0400

    security for user and role api

--- src/main/java/com/sushmita/ork/config/SecurityConfig.java
+++ src/main/java/com/sushmita/ork/config/SecurityConfig.java
@@ -19,45 +19,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 /**
  * @author Sushmita Budhathoki on 2024-08-21
- *
- * TYPES: There are many ways to implement Spring Security - SecurityFilterChain, WebSecurityConfigurerAdapter,
- * Method based, @Preauthorize, @RolesAllowed, @Secured. Depends on the project's need, we choose these.
- *
- * METHODS: SecurityFilterChain is latest in Spring 5.0. Even inside this, we have different ways to handle request
- * to endpoint. CSRF is diabled to CSRF token isnot there for when it is not a web browser. authorizeRequests
- * is to handle access using in the deprecated class, but there is authorizeHttpRequests which is new since
- * Spring 5.0. It allows to add custom filter, lambda expression and is used inside SFC.
- *
- * MECHANISM: For the spring security, we have filter -> authentication manager
- * -> authentication providers -> UserDetailsService
- *
- *
- * Admin
- * - Will Add companies
- * - Will add area, admin can add this
- *
- * Recruiter
- * - Signup based on company
- * - Login
- * - Posts vacancy based on area
- * - Forwards to HR of the company
- * - will create HR and Director user
- *
- * HR
- * - Verifies if the job seeker is eligible (just by looking at)
- * -  Forwards to Director of the company
- *
- * Director
- * - Approves and sets time for interview
- *
- * Applicant
- * - Signup
- * - Login
- * - Sees multiple companies before login, this is PUBLIC
- * - Can apply to the hiring post
- * - Sees the process throughout
- *
- * Same login page, dashboard and navigation is based on role's nav permissions
  */
 
 @Configuration
@@ -77,7 +38,13 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/vacancy/**").hasRole("RECRUITER")
                         .requestMatchers(HttpMethod.POST, "/users/application/create").hasAuthority("APPLICANT_WRITE")
                         .requestMatchers(HttpMethod.POST, "/users/application/stages").hasAnyAuthority("RECRUITER", "HR", "DIRECTOR")
-                        .requestMatchers("/users/**").permitAll()
+
+                        //user
+                        .requestMatchers(HttpMethod.POST,"/users").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/users/register").permitAll()
+
+                        //role
+                        .requestMatchers(HttpMethod.GET, "/roles/all").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(e ->
