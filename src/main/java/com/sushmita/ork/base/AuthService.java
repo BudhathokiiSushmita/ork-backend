1b69f54c97728566e4d9f89cee00185062bef6b2
sushmitabudhathoki <sushmita.budhathoki56@gmail.com>
2024-09-30 06:18:51 -0400

    base method to get role of current logged in user

---
+++ src/main/java/com/sushmita/ork/base/AuthService.java
@@ -0,0 +1,24 @@
+package com.sushmita.ork.base;
+
+import com.sushmita.ork.enums.RoleType;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import javax.management.ServiceNotFoundException;
+import java.util.List;
+
+/**
+ * @author Sushmita Budhathoki on 2024-09-30
+ */
+
+public class AuthService {
+
+   public static RoleType getCurrentRoleType() throws ServiceNotFoundException {
+        try {
+            List<? extends GrantedAuthority> lists = SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream().toList();
+            return RoleType.valueOf(lists.get(0).toString());
+        } catch (Exception e) {
+            throw new ServiceNotFoundException("Not authorized");
+        }
+    }
+}
